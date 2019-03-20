0f728d6a5c8c87b391ced5a78dba97f3805d8841
robinb242 <robin.bertuccelli@gmail.com>
2019-03-20 10:40:31 -0500

    Heroku build

---
+++ client/build/precache-manifest.d745c82eeb9baab22ee7c5d50ec3fc53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71e0b09cbe54c1749950",
+    "url": "/static/css/main.27cf2de5.chunk.css"
+  },
+  {
+    "revision": "71e0b09cbe54c1749950",
+    "url": "/static/js/main.8b669633.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "60eda4a812d2d189c1fa",
+    "url": "/static/js/2.8f11a50c.chunk.js"
+  },
+  {
+    "revision": "eddf1a8258d7963168c4d2e3ef17b462",
+    "url": "/index.html"
+  }
+];
