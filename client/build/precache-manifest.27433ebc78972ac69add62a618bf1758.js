6040a4c0a739ed9d20a98f4e586efd9e402f5942
robinb242 <robin.bertuccelli@gmail.com>
2019-03-27 10:10:12 -0500

    rebuild

---
+++ client/build/precache-manifest.27433ebc78972ac69add62a618bf1758.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9aa5d0d6584ceac8a5ed",
+    "url": "/news-scrape-react.git/static/css/main.27cf2de5.chunk.css"
+  },
+  {
+    "revision": "9aa5d0d6584ceac8a5ed",
+    "url": "/news-scrape-react.git/static/js/main.92858066.chunk.js"
+  },
+  {
+    "revision": "7a6620fb46250e50901f",
+    "url": "/news-scrape-react.git/static/js/runtime~main.cc50d6b3.js"
+  },
+  {
+    "revision": "60eda4a812d2d189c1fa",
+    "url": "/news-scrape-react.git/static/js/2.8f11a50c.chunk.js"
+  },
+  {
+    "revision": "ad943526d54379ec61982deb3e9e8d79",
+    "url": "/news-scrape-react.git/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a761bf67e4b09928a8a89a2a3acbb9e.js"
+  "/news-scrape-react.git/precache-manifest.27433ebc78972ac69add62a618bf1758.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/news-scrape-react.git/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
